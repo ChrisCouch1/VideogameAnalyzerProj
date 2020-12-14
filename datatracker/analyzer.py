5ca6c20031462ec8d7a469397fe1ef0496f5f1a8
larutow <lucas.rutowski@gmail.com>
2020-12-14 13:25:23 -0600

    search results showing in cards

--- datatracker/analyzer.py
+++ datatracker/analyzer.py
@@ -14,13 +14,13 @@ def index():
 
     response = requests.get("https://api.dccresource.com/api/games/5faac562db090e1a5c2dea16")
     games = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
+    foundgame = []
 
     if request.method == 'POST':
         response = requests.get("https://api.dccresource.com/api/games/")
         games = json.loads(response.content, object_hook=lambda d:SimpleNamespace(**d))
 
         user_input = request.form['gametitlesearch']
-        foundgame = []
         for game in games:
             if user_input in game.name:
                 foundgame.append(game)
