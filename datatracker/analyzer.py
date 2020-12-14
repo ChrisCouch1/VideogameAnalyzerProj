ffff3b03693f06f2c9355b85fa691aed2991ae9c
larutow <lucas.rutowski@gmail.com>
2020-12-14 17:18:04 -0600

    analyzer/systems now displays a graph with the sales by system

--- datatracker/analyzer.py
+++ datatracker/analyzer.py
@@ -4,10 +4,36 @@ import requests, json
 
 bp = Blueprint('analyzer', __name__)
 
-
 @bp.route('/test')
 def test():
     return "All good!"
+
+@bp.route('/analyzer/systems', methods=['GET', 'POST'])
+def system_sales():
+    response = requests.get("https://api.dccresource.com/api/games/")
+    games = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
+
+    platform_total_sales = {}
+
+    #PSEUDOCODE FOR PLATFORM SALES CALCULATION
+    #for each game
+        #identify its platform
+        #if new platform
+            #add platform to dict
+        #if existing platform
+            #add num of global sales to dict "platform_total_sales"
+
+    for game in games:
+        platform = game.platform
+        if platform not in platform_total_sales.keys():
+            platform_total_sales.update({platform: game.globalSales})
+        else:
+            platform_total_sales.update({platform: (platform_total_sales[platform] + game.globalSales)})
+
+    return_systems = list(platform_total_sales.keys())
+    return_sales = list(platform_total_sales.values())
+    return render_template('analyzer/systems.html', platforms = return_systems, sales = return_sales)
+
 
 @bp.route('/analyzer', methods=['GET', 'POST'])
 def index():
